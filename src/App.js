ca00391cfe458d327b1fc6f79d53bfc27c646bbd
kimdomenick <kdomenick@thinkoomph.com>
2024-11-01 09:50:15 -0400

    Replace generic React page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Documentation</a>
+            </li>
+            <li>
+              <a href="#">Support</a>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <section class="hero">
+          <h1>Github Commits to JIRA/Tempo Integration</h1>
+          <p>
+            Streamline your workflow by connecting your GitHub commits to JIRA
+            and Tempo.
+          </p>
+          <button>Get Started</button>
+        </section>
+        <section class="features">
+          <h2>Key Features</h2>
+          <ul>
+            <li>Automatically log GitHub commits in JIRA</li>
+            <li>Track time spent on issues in Tempo</li>
+            <li>Seamless integration with existing workflows</li>
+          </ul>
+        </section>
+        <section class="call-to-action">
+          <h2>Start integrating today!</h2>
+          <button>Sign Up</button>
+        </section>
+      </main>
+      <footer>
+        <p>
+          &copy; 2024 Github Commits to JIRA/Tempo Integration.{" "}
+          <a href="https://www.oomphinc.com/" target="_blank">
+            Oomph, Inc.
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
